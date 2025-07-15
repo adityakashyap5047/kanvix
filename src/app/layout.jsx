2322b2cfc367000217ecaf999ba7cd5a762aff4d
adityakashyap5047 <adityakashyap5047@gmail.com>
2025-07-15 18:08:47 +0530

    adding the shadcn

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,7 +1,6 @@
-import type { Metadata } from "next";
 import "./globals.css";
 
-export const metadata: Metadata = {
+export const metadata = {
   title: "Kanvix",
   description: "Kanvix is a modern project management platform designed to simplify team collaboration, track progress effortlessly, and ensure on-time delivery of goals.",
   icons: {
@@ -13,9 +12,7 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+}) {
   return (
     <html lang="en">
       <body
