29f295563a3ecc75aa21b36fa335eca0cfc131d6
adityakashyap5047 <adityakashyap5047@gmail.com>
2025-07-15 18:38:25 +0530

    defining the eslint config

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,15 @@
+import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
 
 export const metadata = {
   title: "Kanvix",
@@ -7,16 +18,14 @@ export const metadata = {
       icon: "kanvix.png",
       shortcut: "kanvix.png",
       apple: "kanvix.png",
-    },
+  },
 };
 
-export default function RootLayout({
-  children,
-}) {
+export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
       </body>
