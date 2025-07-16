99a33b313ff3181d150b13f328e199450af01dd5
adityakashyap5047 <adityakashyap5047@gmail.com>
2025-07-16 18:36:11 +0530

    completed the organization section

--- app/(main)/organization/[orgId]/page.jsx
+++ app/(main)/organization/[orgId]/page.jsx
@@ -1,4 +1,5 @@
 import { getOrganization } from '@/actions/organization';
+import OrgSwitcher from '@/components/org-switcher';
 import React from 'react'
 
 const page = async ({ params }) => {
@@ -12,8 +13,20 @@ const page = async ({ params }) => {
     }
 
     return (
-        <div>
-            <h1>{organization.name}&rsquo;s Projects</h1>
+        <div className='container px-8 mx-auto'>
+            <div className='mb-4 flex flex-col sm:flex-row justify-between items-start'>
+                <h1 className='text-5xl font-bold gradient-title pb-2'>{organization.name}&rsquo;s Projects</h1>
+
+                <div>
+                    <OrgSwitcher />
+                </div>
+            </div>
+            <div className='mb-4'>
+                Show org projects
+            </div>
+            <div className='mt-8'>
+                Show user assigned and reported issues here
+            </div>
         </div>
     )
 }
