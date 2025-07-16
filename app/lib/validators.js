649b05a1364fae05abb45d461fa38aaa5caaa7ba
adityakashyap5047 <adityakashyap5047@gmail.com>
2025-07-16 20:38:13 +0530

    Adding the validators for creating project form

---
+++ app/lib/validators.js
@@ -0,0 +1,16 @@
+import { z } from "zod";
+
+export const projectSchema = z.object({
+    name: z
+        .string()
+        .min(1, "Project name is required")
+        .max(100, "Project name must be less than 100 characters"),
+    key: z
+        .string()
+        .min(2, "Project key is required")
+        .max(10, "Project key must be less than 10 characters"),
+    description: z
+        .string()
+        .max(500, "Project description must be less than 500 characters")
+        .optional(),
+})
