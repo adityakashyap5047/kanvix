f21b1c999122312b19e06cc465d0359b8be2e578
adityakashyap5047 <adityakashyap5047@gmail.com>
2025-07-16 12:11:24 +0530

    created the key-features component

--- app/page.js
+++ app/page.js
@@ -1,7 +1,29 @@
 import { Button } from "@/components/ui/button"
-import { ChevronRight } from "lucide-react"
+import { Card, CardContent } from "@/components/ui/card";
+import { BarChart, Calendar, ChevronRight, Layout } from "lucide-react"
 import Image from "next/image"
 import Link from "next/link"
+
+const features = [
+  {
+    title: "Intuitive Kanban Boards",
+    description:
+      "Visualize your workflow and optimize team productivity with our easy-to-use Kanban boards.",
+    icon: Layout,
+  },
+  {
+    title: "Powerful Sprint Planning",
+    description:
+      "Plan and manage sprints effectively, ensuring your team stays focused on delivering value.",
+    icon: Calendar,
+  },
+  {
+    title: "Comprehensive Reporting",
+    description:
+      "Gain insights into your team's performance with detailed, customizable reports and analytics.",
+    icon: BarChart,
+  },
+];
 
 function page() {
   return (
@@ -31,6 +53,26 @@ function page() {
             Learn More
           </Button>
         </Link>
+      </section>
+
+      {/* Features Section */}
+      <section id="features" className="bg-gray-900 py-20 px-5">
+        <div className="container mx-auto">
+          <h3 className="text-3xl font-bold mb-12 text-center">Key Features</h3>
+          <div className="grid grid-cols-1 lg:grid-cols-3 gap-8">
+            {features.map((feature, index) => (
+              <Card key={index} className="bg-gray-800">
+                <CardContent className="pt-6">
+                  <feature.icon className="h-12 w-12 mb-4 text-blue-300" />
+                  <h4 className="text-xl font-semibold mb-2">
+                    {feature.title}
+                  </h4>
+                  <p className="text-gray-300">{feature.description}</p>
+                </CardContent>
+              </Card>
+            ))}
+          </div>
+        </div>
       </section>
     </div>
   )
