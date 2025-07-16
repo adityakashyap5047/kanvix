b12ced9ea1995eb852f89c0ca3fd2d5a029d3228
adityakashyap5047 <adityakashyap5047@gmail.com>
2025-07-16 20:50:57 +0530

    customizing the project page for unauthorized user

--- app/(main)/project/create/page.jsx
+++ app/(main)/project/create/page.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
+"use client";
 
-const page = () => {
+import OrgSwitcher from "@/components/org-switcher";
+import { useOrganization, useUser } from "@clerk/nextjs";
+import { useEffect, useState } from "react";
+
+const CreateProjectPage = () => {
+
+  const { isLoaded: isOrgLoaded, membership } = useOrganization();
+  const { isLoaded: isUserLoaded } = useUser();
+  const [isAdmin, setIsAdmin] = useState(false);
+
+  useEffect(() => {
+    if(isOrgLoaded && isUserLoaded && membership) {
+      setIsAdmin(membership.role === "org:admin");
+    }
+  }, [isOrgLoaded, isUserLoaded, membership]);
+
+  if (!isOrgLoaded || !isUserLoaded) {
+    return null;
+  }
+
+  if(!isAdmin){
+    return(
+      <div className="flex flex-col gap-4 items-center">
+        <span className="text-2xl gradient-title">Oops! Only Admins can create project Schema.</span>
+        <OrgSwitcher />
+      </div>
+    )
+  }
+
   return (
-    <div>page</div>
+    <div>CreateProjectPage</div>
   )
 }
 
-export default page
+export default CreateProjectPage
