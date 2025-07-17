6985ed65eb08db4f868d4fb0deef2d3a7b94682d
adityakashyap5047 <adityakashyap5047@gmail.com>
2025-07-17 14:01:25 +0530

    displaying project by id

--- app/(main)/project/[projId]/layout.js
+++ app/(main)/project/[projId]/layout.js
@@ -1,7 +1,11 @@
+import { Suspense } from "react"
+
 const ProjectLayout = ({children}) => {
   return (
-    <div>
-      {children}
+    <div className="mx-auto">
+        <Suspense fallback={<span>Loading Projects...</span>}>
+          {children}
+        </Suspense>
     </div>
   )
 }
--- app/(main)/project/[projId]/page.jsx
+++ app/(main)/project/[projId]/page.jsx
@@ -1,11 +1,34 @@
+import { getProject } from '@/actions/project';
+import NotFound from '@/app/not-found';
 import React from 'react'
 
 const page = async ({params}) => {
 
   const projId = await params?.projId;
 
+  const project = await getProject(projId);
+
+  if(!project) {
+    return NotFound();
+  }
+
   return (
-    <div>page {projId}</div>
+    <div className='container mx-auto xl:max-w-[95%]'>
+      {/* Sprint Creation  */}
+      <SprintCreationForm
+        projectTitle={project.name}
+        projectId={projId}
+        projectKey={project.key}
+        sprintKey={project.sprints?.length + 1}
+      />
+
+      {/* sprint Board  */}
+      {project.sprints.length > 0 ? (
+        <></>
+      ) : (
+        <div>Create a Sprint From Above Button</div>
+      )}
+    </div>
   )
 }
 
