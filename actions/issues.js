276c163a605e7969be757b73e90257d2b8e179e3
adityakashyap5047 <adityakashyap5047@gmail.com>
2025-07-18 09:24:55 +0530

    creating the actions for updating the issue

--- actions/issues.js
+++ actions/issues.js
@@ -131,3 +131,59 @@ export async function deleteIssue(issueId) {
 
     return {success: true};
 }
+
+export async function updateIssue(issueId, data) {
+    const {userId, sessionClaims} = auth();
+        
+    const orgId = sessionClaims?.o?.id;
+
+    if (!userId || !orgId) {
+        throw new Error("Unauthorized");        
+    }
+
+    const user = await db.user.findUnique({
+        where: {clerkUserId: userId},
+    });
+
+    if (!user) {
+        throw new Error("User not found");
+    }
+
+    try {
+        const issue = await db.issue.findUnique({
+            where: {id: issueId},
+            include: {project: true}
+        });
+
+        if (!issue) {
+            throw new Error("Issue not found");
+        }
+
+        if (issue.project.organizationId !== orgId) {
+            throw new Error("Unauthorized to update this issue");
+        }
+
+        if(
+            issue.reporterId !== user.id &&
+            !issue.project.adminIds.includes(user.id)
+        ){
+            throw new Error("Unauthorized to delete this issue");
+        }
+
+        const updateIssue = await db.issue.update({
+            where: {id: issueId},
+            data: {
+                status: data.status,
+                priority: data.priority
+            },
+            include: {
+                reporter: true,
+                assignee: true,
+            }
+        });
+
+        return updateIssue;
+    } catch (error) {
+        throw new Error(`Error updating issue: ${error.message}`);
+    }
+}
